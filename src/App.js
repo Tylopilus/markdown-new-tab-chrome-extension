763ca4ca77bb7d03b0837cc13cc4104400aaa177
Helge <elitaer@gmail.com>
2020-04-07 20:21:09 +0200

    add contextProvider

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.scss';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { TextComponent } from './components/TextComponent';
 import { Menu } from './components/Menu.tsx';
+import { Context } from './context';
 
 function App() {
   return (
     <Router>
       <main>
-        <Menu />
-        <TextComponent />
+        <Context.Provider>
+          <Menu />
+          <TextComponent />
+        </Context.Provider>
       </main>
     </Router>
   );
